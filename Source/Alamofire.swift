126eba44401039310d207c66d7768173f0a338a3
HungNguyen <robert@rubify.com>
2017-03-15 15:42:56 +0700

    add space

--- Source/Alamofire.swift
+++ Source/Alamofire.swift
@@ -21,7 +21,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 //
-
+  
 import Foundation
 
 /// Types adopting the `URLConvertible` protocol can be used to construct URLs, which are then used to construct
